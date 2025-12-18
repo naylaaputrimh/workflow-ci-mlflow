c8e46581b93374136f9b7f685021a5762595532c
naylaaputrimh <nayput1911@gmail.com>
2025-12-18 11:12:23 +0700

    Add model training and MLflow logging

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,1 +1,40 @@
-print("MLflow CI workflow running")
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+df = pd.read_csv("telco_processed.csv")
+
+# Pisahkan fitur dan target
+X = df.drop("Churn", axis=1)
+y = df["Churn"]
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Pipeline
+pipeline = Pipeline([
+    ("scaler", StandardScaler()),
+    ("model", LogisticRegression(max_iter=1000))
+])
+
+with mlflow.start_run():
+    # Train
+    pipeline.fit(X_train, y_train)
+
+    # Predict
+    y_pred = pipeline.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    # Log ke MLflow
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(pipeline, "model")
+
+    print(f"Training selesai. Accuracy: {acc:.4f}")
